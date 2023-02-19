834d6766c383c6b0485eadde25881644764a8b0b
Cleiton Araújo <cltn.info@gmail.com>
2023-02-19 16:08:18 -0300

    Projeto inicial

---
+++ Controllers/ArquivosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobStorageApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArquivosController : ControllerBase
+    {
+       
+    }
+}
